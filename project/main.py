3250a51d4bb72bd9b6b209db9b163092a5280714
CyberSanya2008 <derzhaev59@gmail.com>
2020-09-18 22:06:18 +0700

    User registration

--- project/main.py
+++ project/main.py
@@ -1,21 +1,44 @@
 from flask import Blueprint, request
-from .models import User, Jopa
+from .models import User
 from . import db
+import json
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
-    return 'Index'
-
-
-@main.route('/registration', methods=['POST'])
-def registration():
     data = request.data
     print(data)
 
-    return registration
+    return 'index'
+
+
+# Регистрация пользователя и добавление его в базу данных
+@main.route('/registration', methods=['POST'])
+def registration():
+
+    data = request.data
+    parsed_data = json.loads(data)
+
+    email = parsed_data['email']
+    name = parsed_data['name']
+    password = parsed_data['password']
+
+    # Проверка email
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return redirect('/')
+
+    # Добавление Пользователя, если email не существует в бд
+    new_user = new_user = User(email=email, name=name, password=password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    print(data)
+
+    return 'registration'
 
 
 @main.route('/profile')

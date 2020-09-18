cdf8d2e9f3bc0c8949fd8402a7c428ba132db419
CyberSanya2008 <derzhaev59@gmail.com>
2020-09-18 18:28:10 +0700

    Route for registration

--- project/main.py
+++ project/main.py
@@ -1,12 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from .models import User, Jopa
 from . import db
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def index():
     return 'Index'
+
+
+@main.route('/registration', methods=['POST'])
+def registration():
+    data = request.data
+    print(data)
+
+    return registration
+
 
 @main.route('/profile')
 def profile():

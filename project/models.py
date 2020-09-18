7b0b241cfe77ff6845d45df6a5e0c054aed393af
CyberSanya2008 <derzhaev59@gmail.com>
2020-09-18 22:08:50 +0700

    Prettier

--- project/models.py
+++ project/models.py
@@ -1,11 +1,9 @@
-# models.py
-
-
 from . import db
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-

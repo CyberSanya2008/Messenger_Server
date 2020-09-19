177afdf6ed93a11dd2e29972784823842c8fa14a
CyberSanya2008 <derzhaev59@gmail.com>
2020-09-19 18:45:50 +0700

    Add login route

--- project/main.py
+++ project/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
 from .models import User
 from . import db
 import json
@@ -32,7 +33,8 @@ def registration():
         return redirect('/')
 
     # Добавление Пользователя, если email не существует в бд
-    new_user = new_user = User(email=email, name=name, password=generate_password_hash(password))
+    new_user = new_user = User(
+        email=email, name=name, password=generate_password_hash(password))
 
     db.session.add(new_user)
     db.session.commit()
@@ -42,6 +44,35 @@ def registration():
     return 'registration'
 
 
+@main.route('/login', methods=['POST'])
+def login_post():
+    data = request.data
+    parsed_data = json.loads(data)
+
+    email = parsed_data['email']
+    password = parsed_data['password']
+    checkbox = parsed_data['checkbox']
+
+    if checkbox == "True":
+        remember = True
+    else:
+        remember = False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        print("not")
+        return redirect('/')
+
+    login_user(user, remember=remember)
+    return redirect('/profile')
+
+    # print(email, password, checkbox)
+
+    return ""
+
+
 @main.route('/profile')
+@login_required
 def profile():
-    return 'Profile'
+    return ("jopa")

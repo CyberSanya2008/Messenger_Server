015e3b047233ba9b0941950ff054b980c786c9ab
CyberSanya2008 <derzhaev59@gmail.com>
2020-09-19 18:46:24 +0700

    Add UserMixin

--- project/models.py
+++ project/models.py
@@ -1,7 +1,8 @@
+from flask_login import UserMixin
 from . import db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     # primary keys are required by SQLAlchemy
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)

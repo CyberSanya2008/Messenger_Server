d2e6392b3e01736f7dc7768e86070f386c6cf374
CyberSanya2008 <derzhaev59@gmail.com>
2020-09-21 20:40:05 +0700

    Add Messages model

--- project/models.py
+++ project/models.py
@@ -2,9 +2,16 @@ from flask_login import UserMixin
 from . import db
 
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     # primary keys are required by SQLAlchemy
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    sender_id = db.Column(db.Integer)
+    receiver_id = db.Column(db.Integer)
